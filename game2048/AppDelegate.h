f952ca641899634f0c3b19c525af77fd2647a854
SingYi <23323231@qq.com>
2016-06-30 23:41:54 +0800

    Initial Commit

---
+++ game2048/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  game2048
+//
+//  Created by 石燚 on 16/6/30.
+//  Copyright © 2016年 SingYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
